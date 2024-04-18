8bdff195df6aea1bc2078b016e3bdb1da18cefe0
AdrianOTerra <adrian.ortter@sanviatorvalladolid.com>
2024-04-18 10:11:44 +0200

    funcion calculadora

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,33 @@ public class Principal {
 	public static void main(String[] args) {
 		
 
+		calculadura(funcion, numeros[0], numeros[1]);
+
+		
+		
+
+	}
+
+	public static double calculadora(int opcion, int num1, int num2){
+		
+		double resultado = 0;
+		switch(opcion){
+		
+		case 1:
+		resultado = num1+num2;
+			break;
+		case 2:
+		resultado = num1-num2;
+			break;
+		case 3:
+		resultado = num1/num2;
+			break;
+		case 4:
+		resultado = num1*num2;
+			break;
+		}
+
+		return resultado;
 	}
 
 }
